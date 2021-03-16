d1c207ae262bf16ed1e39cc4f66b6a719d536d09
Sara <sarisilvolopez1991@gmail.com>
2021-03-16 21:04:29 +0100

    update public_id image

--- src/main/java/com/example/demo/domain/Image/Image.java
+++ src/main/java/com/example/demo/domain/Image/Image.java
@@ -1,13 +1,11 @@
 package com.example.demo.domain.Image;
 
-import java.util.UUID;
-
 import javax.persistence.Embeddable;
 
 
 @Embeddable
 public class Image {
     public String asset_id;
-    public UUID public_id;
+    public String public_id;
     public String url;
 }
---
+++ src/main/java/com/example/demo/domain/Image/update pizza.sql
@@ -0,0 +1,2 @@
+update pizza
+set public_id = 'lkrgaz0rsp3iuhdrq5s7'
